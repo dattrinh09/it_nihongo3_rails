be807c7077d937b5a30956728e406ce350ebb447
dattrinh09 <tiendattrinh09@gmail.com>
2021-12-09 03:28:47 +0000

    Add avatar on users

--- thefirstrails/app/controllers/application_controller.rb
+++ thefirstrails/app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :age, :email, :password)}
         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :age, :email, :password, :current_password)}
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :age, :avatar, :email, :password)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :avatar, :age, :email, :password, :current_password)}
     end
 end
